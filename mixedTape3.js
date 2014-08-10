0475508728927c88396c6806e0df3ea6996e8b7a
kelichiu <keli.chiu.kc@gmail.com>
2014-08-10 10:11:00 -0400

    playlist items display max number 50

--- mixedTape3.js
+++ mixedTape3.js
@@ -1,44 +1,23 @@
-//https://www.googleapis.com/youtube/v3/playlists
-//AIzaSyAmkH9BharZeRdnAgEawsvU3miif40CgJc
-//https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyAJilIGstSZj3XljQMtpg5GhxoksVqPGkM&part=snippet,contentDetails,statistics,status
 var mixedTapeApp = {}
 mixedTapeApp.appData = {},
 mixedTapeApp.apikey = 'AIzaSyAJilIGstSZj3XljQMtpg5GhxoksVqPGkM';
 var playlistId = $('#playlistId')[0].value;
-//global function that loads the player
-var player;
-$('#bt_play').on('click', function(){
-  	player.playVideo();
-  });
-  $('#bt_pause').on('click', function(){
-  	player.pauseVideo();
-  });
-  $('#bt_stop').on('click', function(){
-  	player.stopVideo();
-  });
-
-
-
-  
 
 
 mixedTapeApp.init = function(playlistId){
 	var playlistId = $('#playlistId').val();
 		mixedTapeApp.getPlaylist(playlistId);
 		mixedTapeApp.getPlaylistItems(playlistId);	
-		mixedTapeApp.loadAPI(playlistId);
-		mixedTapeApp.onYouTubePlayerAPIReady(playlistId);
-
 	},
 
 mixedTapeApp.getPlaylist = function(playlistId){
-
 	//Get playlist ID from user input
 	// var playlistId = $('#playlistId').val(); this is not necessary when I use "playlistId" as a parameter
+	
+	//define request HTTP from the user input of playlistId
 	var playlistAPI = "https://www.googleapis.com/youtube/v3/playlists?part=snippet,player&id=" + playlistId + "&key=" + mixedTapeApp.apikey;
  
 	$.ajax({
-			//url:'https://www.googleapis.com/youtube/v3/playlists?part=snippet,player&id=FLHJsepw-FGo__FuTphk2U_g&key=AIzaSyAJilIGstSZj3XljQMtpg5GhxoksVqPGkM',
 			url: playlistAPI,
 			type:'GET',
 			key:mixedTapeApp.apikey,
@@ -52,6 +31,7 @@ mixedTapeApp.getPlaylist = function(playlistId){
 
 };
 
+//storing response
 mixedTapeApp.parseDataPlaylist = function(response){
 	//console.log(response.items[0]);
 	var playlistInfo = response.items[0].snippet;
@@ -65,6 +45,7 @@ mixedTapeApp.parseDataPlaylist = function(response){
 	mixedTapeApp.updateDom();
 }
 
+//print response to HTML
 mixedTapeApp.updateDom = function(){
 	//update data to HTML document
 	$('#title').html(mixedTapeApp.appData.title);
@@ -72,105 +53,48 @@ mixedTapeApp.updateDom = function(){
 	$('#description').html(mixedTapeApp.appData.description);
 	//$('#frame').html(mixedTapeApp.appData.videosStream);
 };
-//get playlist songs' thumbnails and titles
+
+
+
 mixedTapeApp.itemsDisplay={};
 mixedTapeApp.itemsData = {};
 
-mixedTapeApp.getPlaylistItems = function(){
-	var playlistId = $('#playlistId').val();
-	var playlistItemsAPI = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId='+playlistId+'&key='+mixedTapeApp.apikey;
+//get videos's thumbnails and titles from playlistitems API
+mixedTapeApp.getPlaylistItems = function(playlistId){
+	var playlistItemsAPI = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&maxResults=50&playlistId='+playlistId+'&key='+mixedTapeApp.apikey;
 	console.log(playlistItemsAPI);
 
 	$.ajax({
-			//https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PL3_NLXp9puXWs19A9mdPXkoQ_WZ4IEBvp&key=AIzaSyAJilIGstSZj3XljQMtpg5GhxoksVqPGkM
-
 			url: playlistItemsAPI,
 			type:'GET',
 			key:mixedTapeApp.apikey,
 			dataType: 'jsonp', 
+			maxResults:10,
 			success: function(result){
 				mixedTapeApp.displayThumb(result.items);
 				console.log(result.items);
+				nextPageToken = result.nextPageToken;			    
 			}
-
 		});
-	mixedTapeApp.displayThumb = function(result){
+
+//Display thumbnails and titles on HTML
+mixedTapeApp.displayThumb = function(result){
 	$.each(result,function(i, thumb){
 		var title = $('<h2>').text(thumb.snippet.title);
 		var image = $('<img>').attr('src', thumb.snippet.thumbnails.default.url);
 		var thumbList = $('<div>').addClass('thumbnail').append(title, image);
 		$('#thumbnails').append(thumbList);
-
-	})
-	
-		
-		
-	
+		})
+	};
 };
-
-};
-
-
-
-
-
-///////////////////////////////////////////////
-//mixedTapeApp.playerLoad = function(){
-	//mixedTapeApp.loadAPI();
-//};
-
-
-// Load the IFrame Player API code asynchronously.
-mixedTapeApp.loadAPI = function(){
-	var tag = document.createElement('script');
-	tag.src = "https://www.youtube.com/player_api";
-	var firstScriptTag = document.getElementsByTagName('script')[0];
-	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-	console.log('API is loading');
-
-  
-};
-  
-// Replace the 'ytplayer' element with an <iframe> and
-  // YouTube player after the API code downloads. 
-  function onYouTubePlayerAPIReady(playlistId) {
-	var playlistId = $('#playlistId')[0].value;
-    player = new YT.Player('ytplayer', {
-      height: '390',
-      width: '640',
-      playerVars:{
-      	controls:0,
-      	listType: 'playlist',
-      	list: playlistId
-      }
-
-    });  
-    console.log(playlistId);
-  };
-
-  mixedTapeApp.onYouTubePlayerAPIReady = function(playlistId){
-  	//var playlistId = $('#playlistId')[0].value;
-  	onYouTubePlayerAPIReady(playlistId);
-  	console.log('Player is ready!');
-
-  };
-
-
-
-
 
 $(function(){
 	//Initialize mixedTape app when user click the button to pass their playlist ID
 	$('#submit').on('click',function(e){
 		e.preventDefault();
-		var playlistId = $('#playlistId')[0].value;
 		mixedTapeApp.init(playlistId);
 		$('#thumbnails').empty();
-	
 
-
-
-		
 	});
 });
 
---
+++ player.js
@@ -0,0 +1,50 @@
+// Load the IFrame Player API code asynchronously.
+	function loadAPI(){
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/player_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  console.log('Plater API is loading');
+  };
+
+// Replace the 'ytplayer' element with an <iframe> and
+  // YouTube player after the API code downloads.   
+  var player;
+  function onYouTubePlayerAPIReady() {
+	var playlistId = $('#playlistId')[0].value;
+    player = new YT.Player('ytplayer', {
+      height: '390',
+      width: '640',
+      playerVars:{
+      	controls:0,
+      	listType: 'playlist',
+      	list: playlistId
+      },
+    });  
+    console.log(playlistId);
+  };
+
+
+//customized control buttons
+$('#bt_play').on('click', function(){
+    player.playVideo();
+  });
+  $('#bt_pause').on('click', function(){
+    player.pauseVideo();
+  });
+  $('#bt_stop').on('click', function(){
+    player.stopVideo();
+  });
+
+$(function(){
+  $('#submit').on('click',function(e){
+    e.preventDefault();
+    loadAPI();
+    onYouTubePlayerAPIReady();
+    
+  });
+});
+
+  
+
+
